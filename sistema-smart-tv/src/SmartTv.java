0bdeb9926ee90c566daa0df950c410b57159c8b0
rinka04 <matheushenrique1704@gmail.com>
2022-09-20 23:19:28 -0300

    Todos os Erros ainda não foram resolvidos

--- sistema-smart-tv/src/SmartTv.java
+++ sistema-smart-tv/src/SmartTv.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+
 public class SmartTv {
-    public static void main(String[] args) throws Exception {
-        
+   Scanner sc = new Scanner (System.in);
+    boolean ligada = false;
+    int canal, volume;
+    
+
+    public void mudarCanal(int novoCanal){
+        canal = novoCanal;
+    }
+
+    public void aumentarCanal() {
+        canal++;
+    }
+
+    public void diminuirCanal() {
+        canal--;
+    }
+
+    public void ligar() {
+        ligada = true;
+    }
+
+    public void desligar() {
+        desligada = false;
+    }
+
+    public void aumentarVolume() {
+        volume++;
+    }
+
+    public void diminuirVolume() {
+        volume--;
     }
 }
--- sistema-smart-tv/src/Usuario.java
+++ sistema-smart-tv/src/Usuario.java
@@ -1,3 +1,24 @@
 public class Usuario {
+    public static void main(String[] args) {
+        SmartTv smartTv = new SmartTv();
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
     
+        System.out.println("Canal Atual:  " + smartTv.canal);
+
+        smartTv.mudarCanal(15);
+
+        System.out.println("Canal Atual:  " + smartTv.canal);
+
+        System.out.println("TV Ligada?  " + smartTv.ligada);
+
+        System.out.println("Volume Atual:  " + smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("Novo Status -> Tv Ligada?"  + smartTv.ligada);
+
+        
+    }
 }
